479c5c24c21a04045a1df0aa0e61eba5ae8b77e4
misdirectedpuffin <misdirectedpuffin@gmail.com>
2018-06-08 21:18:42 +0100

    Configure cli cutter

--- {{cookiecutter.package_name}}/setup.py
+++ {{cookiecutter.package_name}}/setup.py
@@ -1,20 +1,34 @@
-import setuptools
+from setuptools import setup, find_packages
 
-setuptools.setup(
+try:
+    from pip._internal.req import parse_requirements
+except ImportError:
+    from pip.req import parse_requirements
+
+
+def get_requirements(file):
+    """Return a list of requirements from a file."""
+    requirements = parse_requirements(file, session=False)
+    return [str(ir.req) for ir in requirements if not None]
+
+
+setup(
     name="{{ cookiecutter.package_name }}",
     version="{{ cookiecutter.package_version }}",
     url="{{ cookiecutter.package_url }}",
-
     author="{{ cookiecutter.author_name }}",
     author_email="{{ cookiecutter.author_email }}",
-
     description="{{ cookiecutter.package_description }}",
     long_description=open('README.rst').read(),
-
-    packages=setuptools.find_packages(),
-
-    install_requires=[],
-
+    packages=find_packages(),
+    entry_points={
+        'console_scripts': [
+            '{{ cookiecutter.cli_entrypoint }}={{ cookiecutter.package_name }}.cli:entrypoint',
+        ],
+    },
+    install_requires=get_requirements('requirements.txt'),
+    tests_require=get_requirements('requirements_dev.txt'),
+    setup_requires=['pytest-runner'],
     classifiers=[
         'Development Status :: 2 - Pre-Alpha',
         'Programming Language :: Python',
@@ -24,4 +38,7 @@ setuptools.setup(
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
     ],
+    zip_safe=False,
+    name='{{ cookiecutter.package_name }}',
+    keywords='{{ cookiecutter.package_name }} {{ cookiecutter.cli_entrypoint }}'
 )
